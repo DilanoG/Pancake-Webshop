e44e95cbaae362957509380cd5c31c1bdf8833f6
Dilano <99041735@mydavinci.nl>
2019-10-29 00:12:20 +0100

    fixed homepage design

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -5,13 +5,13 @@
 	<link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
 </head>
 <body>
-	<div class="w-7/12 bg-pink-200 m-auto text-white text-4xl" style="height: 500px">
-		<div class="">
-			<p class="">The Best</p>
+	<div class="w-8/12 bg-pink-200 m-auto text-white text-5xl" style="height: 700px">
+		<div class="pt-40 ml-32" style="font-family: palatino linotype, serif;">
+			<p >The Best</p>
 			<p>Pancakes</p>
 			<p>In The World.</p>
 		</div>
-		<button class="text-black text-base px-3 py-2 ml-20 mt-40 bg-white">Shop Now</button>
+		<button class="text-black text-base px-4 py-2 ml-32 mt-5 bg-white" style="font-family: Futura, Trebuchet MS, Arial, serif;" href="{{ url('/shop')}}">Shop Now</button>
 	</div>
 </body>
 </html>
