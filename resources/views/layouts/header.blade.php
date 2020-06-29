72952b8285f540e290eb7c85ff055f90999bf834
Dilano <99041735@mydavinci.nl>
2020-06-30 01:26:59 +0200

    changed design

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -24,21 +24,19 @@
 
     </head>
     <div class="m-auto flex items-center justify-center py-8">
-        <ul class="font-hairline text-sm">
-            <li class="inline-block mx-5"><a href="{{ url('/home')}}">HOME</a></li>
-            <li class="inline-block mx-5"><a href="{{ url('/shop')}}">SHOP</a></li>
-            <li class="inline-block mx-5"><a href="{{ url('/home')}}">ABOUT ME</a></li>
-            <li class="inline-block mx-5"><a href="{{ url('/home')}}">CONTACT</a></li>
-        </ul>
-        <h1 class="text-3xl mx-10" style="font-family: Palatino Linotype, serif; margin-right: 180px;">Webshop</h1>
-        <div class="row font-hairline">
+        <ul class="font-hairline">
+            <li class="inline-block mx-5"><a href="{{ url('/home')}}">Home</a></li>
+            <li class="inline-block mx-5"><a href="{{ url('/shop')}}">Shop</a></li>
+            <li class="inline-block mx-5"><a href="{{ url('/home')}}">About Me</a></li>
+            <li class="inline-block mx-32 text-3xl" style="font-family: Palatino Linotype, serif;">Webshop</li>
             @if (Auth::check()) 
-                    <p class="inline-block mx-5"><a href="{{ url('/profile')}}">{{ auth()->user()->name }}</a></p>
-                    <p class="inline-block mx-5"><a href="{{ url('/logout')}}">Logout</a></p>
-                    <p class="inline-block mx-5"><a href="{{ url('/cart')}}">Cart</a></p>
+                    <p class="inline-block mx-5 text-base"><a href="{{ url('/profile')}}">{{ auth()->user()->name }}</a></p>
+                    <p class="inline-block mx-5 text-base"><a href="{{ url('/logout')}}">Logout</a></p>
+                    <p class="inline-block mx-5 text-base"><a href="{{ url('/cart')}}">Cart</a></p>
                 @else
-                    <a class="text-black no-underline hover:underline my-auto" href="{{ url('/login')}}">Login</a>
-                    <a class="text-black no-underline hover:underline my-auto" href="{{ url('/register')}}">Register</a>
+                    <a class="text-black no-underline hover:underline my-auto" href="{{ url('/login')}}">LOGIN</a>
+                    <a class="text-black no-underline hover:underline my-auto" href="{{ url('/register')}}">REGISTER</a>
             @endif
+            </ul>
         </div>
     </div>
